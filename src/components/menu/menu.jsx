f094596f3a51edbc906457a691ce70436044e5af
RenanFernandoRuiz <renanfernandor908@gmail.com>
2023-09-01 14:14:46 -0300

    responsividade concluida

---
+++ src/components/menu/menu.jsx
@@ -0,0 +1,11 @@
+import styles from "./menu.module.css"
+
+const Menu = () => {
+    return(
+        <div>
+        
+        </div>
+    )
+}
+
+export default Menu;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,6 +4,8 @@ import logo from "../assets/devflix.png"
 import searchIcon from "../assets/search.svg"
 import "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
 
+
+
 import "./App.css";
 import MovieCard from "../components/movieCard/movieCard";
 import Footer from "../components/footer/footer";
@@ -36,14 +38,11 @@ const App = () => {
         <div id="app">
             <div className="logo">DEVFLIX</div>
             <div className="search">
-                <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} onKeyDown={handlekeyPress} placeholder="Pesquise por filmes" />
-
-                <div>
             <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 90 90" fill="none">
                 <path d="M15 22.5H75V30H15V22.5ZM15 41.25H75V48.75H15V41.25ZM75 60H15V67.5H75V60Z" fill="white"/>
             </svg>
-            </div>
 
+              <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} onKeyDown={handlekeyPress} placeholder="Pesquise por filmes" />
                 <img src={searchIcon} alt="Icone de pesquisa" onClick={() => searchMovies(searchTerm)}  />
 
             </div>
